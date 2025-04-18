9b2f4439f8a250ff1700479bffcabd79974fb725
Swarajit Dey <97842472+Sd2k3@users.noreply.github.com>
2025-04-18 16:50:09 +0530

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,9 +7,9 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       {/* Purple glow effect */}
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[800px] h-[800px] bg-purple-500/20 rounded-full blur-[150px] pointer-events-none"></div>
       
-      <div className="relative z-10 max-w-6xl mx-auto p-4">
+      <div className="relative z-10 w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <Navbar/>
-        <main>{children}</main>
+        <main className="pb-20">{children}</main>
       </div>
     </div>
   );
